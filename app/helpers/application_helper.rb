4782fc1a64246723a44757bf8acad4be0f20ff9c
Noboru Inoue <nobox3@gmail.com>
2025-06-18 23:33:26 +0900

    Refactor custom_meta_tags method: extract i18n_meta_for_path and sanitize_description for improved readability and maintainability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,36 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def custom_meta_tags(options = {})
+  def i18n_meta_for_path
     i18n_params_for_path = props[:i18n_params_for_path]
 
-    i18n_meta = t(
-      i18n_params_for_path[:key],
+    t(i18n_params_for_path[:key],
       **i18n_params_for_path[:interpolation],
       deep_interpolation: true,
       scope: :meta,
-      default: :default,
-    )
+      default: :default)
+  end
 
+  def sanitize_description(text)
+    text.to_s.gsub(/\s+/, ' ').strip
+  end
+
+  def sanitized_canonical_url
+    filtered_query = request.query_parameters.slice(*DefaultValues::PERMITTED_SEARCH_PARAMS)
+    canonical_url = request.base_url + request.path
+    canonical_url += "?#{filtered_query.to_query}" if filtered_query.present?
+    canonical_url
+  end
+
+  def custom_meta_tags(options = {})
+    i18n_meta = i18n_meta_for_path
     title = i18n_meta[:title]
-    description = i18n_meta[:description]
+    description = sanitize_description(i18n_meta[:description])
 
     # meta = decorator&.meta
     # image = meta[:image].presence || image_url('/images/ogp_share.png')
 
-    filtered_query = request.query_parameters.slice(*DefaultValues::PERMITTED_SEARCH_PARAMS)
-    canonical_url = request.base_url + request.path
-    canonical_url += "?#{filtered_query.to_query}" if filtered_query.present?
+    canonical_url = sanitized_canonical_url
     site_name = DefaultValues::SITE_NAME
 
     # https://github.com/kpumuk/meta-tags/
