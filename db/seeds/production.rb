65dcb36f3f0446ceecb1eeeae3ae8ac50c15c5c6
Noboru Inoue <nobox3@gmail.com>
2025-07-14 14:17:30 +0900

    Refactor user creation seed to enable avatar attachment and improve code readability

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -23,7 +23,7 @@ admin.update!(confirmed_at: base_timestamp)
 
 puts 'Create users...'
 
-# fixture_path = Rails.root.join('spec', 'fixtures')
+fixture_path = Rails.root.join('spec', 'fixtures')
 
 users = Array.new(1) do |i|
   num = i + 1
@@ -36,12 +36,56 @@ users = Array.new(1) do |i|
     updated_at: base_timestamp,
   )
 
-  # if i < 8
-  #   user.avatar.attach(
-  #     io: fixture_path.join('avatars', "avatar_#{num}.webp").open, filename: 'avatar.webp',
-  #   )
-  # end
+  if i < 8
+    user.avatar.attach(
+      io: fixture_path.join('avatars', "avatar_#{num}.webp").open, filename: 'avatar.webp',
+    )
+  end
 
   user.update!(confirmed_at: base_timestamp)
   user
+end
+
+puts 'Create relationships...'
+7.times do |i|
+  other_user = users[i + 1]
+
+  users[0].active_relationships.create(followed: other_user)
+  other_user.active_relationships.create(followed: users[0])
+end
+
+puts 'Create posts...'
+50.times do |i|
+  num = i + 1
+  timestamp = base_timestamp + (num * 3.minutes)
+
+  post = users[0].posts.create!(
+    body: Faker::Lorem.sentence(word_count: 10),
+    created_at: timestamp,
+    updated_at: timestamp,
+  )
+
+  if i >= 46
+    post.images.attach(
+      io: fixture_path.join('images', "image_#{num - 46}.webp").open,
+      filename: 'image.webp',
+    )
+  end
+
+  post.update!(sent_at: timestamp)
+end
+
+users[1..].each do |user|
+  10.times do |i|
+    num = i + 1
+    timestamp = base_timestamp + (num * 3.minutes)
+
+    post = user.posts.create!(
+      body: Faker::Lorem.sentence(word_count: 10),
+      created_at: timestamp,
+      updated_at: timestamp,
+    )
+
+    post.update!(sent_at: timestamp)
+  end
 end
