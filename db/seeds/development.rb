a81f1c243f27319bf5349818303bf0403c8b4d7b
Noboru Inoue <nobox3@gmail.com>
2025-07-15 23:10:03 +0900

    Refactor seed files to utilize post contents from YAML and enhance user post creation logic

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -21,11 +21,11 @@ admin = User.create!(
 
 admin.update!(confirmed_at: base_timestamp)
 
-puts 'Create users...'
-
 fixture_path = Rails.root.join('spec', 'fixtures')
+post_contents = YAML.parse_file(Rails.root.join('db', 'seeds', 'post_contents.yml')).to_ruby.deep_symbolize_keys
 
-users = Array.new(20) do |i|
+puts 'Create users...'
+users = Array.new(post_contents[:users].size) do |i|
   num = i + 1
 
   user = User.create!(
@@ -37,9 +37,9 @@ users = Array.new(20) do |i|
   )
 
   if i < 8
-    user.avatar.attach(
-      io: fixture_path.join('avatars', "avatar_#{num}.webp").open, filename: 'avatar.webp',
-    )
+    path = fixture_path.join('avatars', "avatar_#{num}.webp")
+
+    user.avatar.attach(io: path.open, filename: "avatar#{File.extname(path)}")
   end
 
   user.update!(confirmed_at: base_timestamp)
@@ -47,7 +47,7 @@ users = Array.new(20) do |i|
 end
 
 puts 'Create relationships...'
-7.times do |i|
+(users.size - 1).times do |i|
   other_user = users[i + 1]
 
   users[0].active_relationships.create(followed: other_user)
@@ -55,36 +55,24 @@ puts 'Create relationships...'
 end
 
 puts 'Create posts...'
-50.times do |i|
-  num = i + 1
-  timestamp = base_timestamp + (num * 3.minutes)
-
-  post = users[0].posts.create!(
-    body: Faker::Lorem.sentence(word_count: 10),
-    created_at: timestamp,
-    updated_at: timestamp,
-  )
-
-  if i >= 46
-    post.images.attach(
-      io: fixture_path.join('images', "image_#{num - 46}.webp").open,
-      filename: 'image.webp',
-    )
+users.each_with_index do |user, i|
+  if i < post_contents[:users].size
+    contents = post_contents[:users][i][:posts]
+  else
+    contents = Array.new(10).map { |_| { body: Faker::Lorem.sentence(word_count: 10) } }
   end
 
-  post.update!(sent_at: timestamp)
-end
+  contents.each_with_index do |content, j|
+    num = j + 1
+    timestamp = base_timestamp + (num * rand(10..20).minutes)
+    timestamp += 2.hours if content[:images].present?
+    post = user.posts.create!(body: content[:body], created_at: timestamp, updated_at: timestamp)
 
-users[1..].each do |user|
-  10.times do |i|
-    num = i + 1
-    timestamp = base_timestamp + (num * 3.minutes)
+    content[:images]&.each do |image|
+      path = fixture_path.join('images', image)
 
-    post = user.posts.create!(
-      body: Faker::Lorem.sentence(word_count: 10),
-      created_at: timestamp,
-      updated_at: timestamp,
-    )
+      post.images.attach(io: path.open, filename: "image#{File.extname(path)}")
+    end
 
     post.update!(sent_at: timestamp)
   end
