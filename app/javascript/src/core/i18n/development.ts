ba31f0ad2c63c4ca39bced88e45c8a9eaf733e23
Noboru Inoue <nobox3@gmail.com>
2025-07-18 11:58:42 +0900

    Set backend request options to no-cache in development configuration

--- app/javascript/src/core/i18n/development.ts
+++ app/javascript/src/core/i18n/development.ts
@@ -4,6 +4,7 @@ import { HMRPlugin } from 'i18next-hmr/plugin'
 import { initOptions } from 'src/core/i18n/commonConfig'
 
 initOptions.debug = true
+initOptions.backend && (initOptions.backend.requestOptions = { cache: 'no-cache' })
 i18next.use(new HMRPlugin({ webpack: { client: typeof window !== 'undefined' } }))
 
 void i18next.init(initOptions)
